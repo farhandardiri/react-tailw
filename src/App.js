c16866d32598f74fd3db4e0928c5fecdf452f01e
farhandardiri <faran.f4124n@gmail.com>
2021-09-08 00:05:17 +0700

    card example

--- src/App.js
+++ src/App.js
@@ -26,22 +26,35 @@ function TableItem(){
     <div className="container mx-auto mt-12">
        <h1 className="text-2xl text-blue-700 font-bold mb-3">Mysterious Card</h1>
        <div className="grid grid-cols-4 grid-rows-2 gap-4">
-         <div className="col-span-2 row-span-2 bg-blue-100 text-center p-4">
-          Column 1
+         <div className="col-span-2 row-span-2">
+            <CardTable/>
          </div>
-         <div className="col-span-1 row-span-1 bg-blue-100 text-center p-4">
-          Column 2
+         <div className="col-span-1 row-span-1">
+            <CardTable/>
          </div>
-         <div className="col-span-1 row-span-1 bg-blue-100 text-center p-4">
-          Column 3
+         <div className="col-span-1 row-span-1">
+            <CardTable/>
          </div>
-         <div className="col-span-1 row-span-1 bg-blue-100 text-center p-4">
-          Column 4
+         <div className="col-span-1 row-span-1">
+            <CardTable/>
          </div>
-         <div className="col-span-1 row-span-1 bg-blue-100 text-center p-4">
-          Column 5
+         <div className="col-span-1 row-span-1">
+           <CardTable/>
          </div>
        </div>
+    </div>
+  )
+}
+
+function CardTable(){
+  return(
+   <div className="bg-blue-100 rounded p-4 border-2 border-blue-800">
+                Some misterious card
+
+       <img src="https://picsum.photos/300/200" className="w-full" alt="random"/>
+         <div className="mt-4">
+            <BtnInfo/>
+          </div>
     </div>
   )
 }
